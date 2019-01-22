1bf5d199f5ca35079f0e79bfa056e593e237c7a2
Anna Zelenkova <34278973+AZelenkova@users.noreply.github.com>
2019-01-22 08:47:33 +0300

    Update main.c

--- 2_client/main.c
+++ 2_client/main.c
@@ -1,49 +1,64 @@
-﻿// Client side implementation of UDP client-server model
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+﻿#include<stdio.h>       // perror(), printf(),
+#include<string.h>      // memset(), strlen(), strcmp(), bzero(), bcopy()
+#include<unistd.h>      // chdir(), gethostname(), access(), close()
+#include<stdlib.h>      // exit()
+//#include<sys/types.h>
+//#include<sys/socket.h>
+//#include<netinet/in.h>
+#include<netdb.h> // socket(), connect(), htons(), recv(), send()
 
-#define PORT	 8080
-#define MAXLINE 1024
+/* + Создать гнездо домена INET типа virtual circuit.
+ * + Прочитать сообщение из серверного гнезда.
+ * + Определить имена файлов, которые нельзя выполнять для данного пользователя и
+ * + отправить их в виде сообщения в серверное гнездо.
+ */
 
-// Driver code
-int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from client";
-    struct sockaddr_in	 servaddr;
+int main()
+{
+    struct sockaddr_in kaddr;
+    struct hostent *hp;
+    int sid, num = 0;
+    char buffer[512] = {0};
 
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+    sid = socket(AF_INET, SOCK_STREAM,0);
+    if (sid == -1) exit(1);
+    printf("Клиент\n-----------------\nСокет создан\n");
+
+    bzero((void *)&kaddr, sizeof(kaddr));
+
+    gethostname(buffer, 256);
+    hp=gethostbyname(buffer);
+    bcopy(hp -> h_addr, (struct sockaddr *)&kaddr.sin_addr, (size_t)hp -> h_length);
+    memset(buffer,0,strlen(buffer));
+
+    kaddr.sin_port = htons(8080);
+    kaddr.sin_family = AF_INET;
+
+    int i;
+    for(i = 0; i<10; ++i){
+        if(connect(sid, (struct sockaddr *)&kaddr, sizeof(kaddr)) != 0) sleep(1);
+        else break;
+    }
+    if(i == 10){
+        perror("ошибка соединения с гнездом сервера\n");
+        close(sid);
+        exit(1);
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    printf("Подключение к серверу успешно\n");
+    printf("Жду сообщения\n");
 
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    while((recv(sid, buffer, sizeof(buffer), 0) != -1 && strcmp(buffer,"exit") != 0)){
+        buffer[strlen(buffer)-1] = 0;
+        printf("%s\n",buffer);
+        if(!access(buffer, 1)){
+            send(sid, buffer, sizeof(buffer), 0);
+            ++num;
+        } else send(sid, "", 1, 0);
+        memset(buffer,0,strlen(buffer));
+    }
 
-    int n, len;
+    if(num == 0) send(sid, "\nНет файлов с правами на выполнение\n", 100, 0);
 
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
-
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
-
-    close(sockfd);
-    return 0;
+    close(sid);	//закрываем дескриптор гнезда
 }
